5f5eb1c3ba5ff57cdef53b8d8a3732390b33374e
Kang <rkdguswns0803@gmail.com>
2022-07-03 22:55:20 +0900

    modified CSS, etc.

--- js/todo.js
+++ js/todo.js
@@ -23,7 +23,7 @@ function paintToDo(newTodo) {
   const span = document.createElement('span');
   span.innerText = newTodo.text;
   const button = document.createElement('button');
-  button.innerText = '❌';
+  button.innerText = 'Clear';
 
   button.addEventListener('click', deleteToDo);
 
